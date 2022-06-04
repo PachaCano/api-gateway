942b877cd56d723860d802fa9ab8cbd256c5f004
Leonel <rpacha731@alumnos.iua.edu.ar>
2022-06-04 11:58:47 -0300

    Initial commit

---
+++ src/main/java/com/pc/apigateway/controllers/HealthController.java
@@ -0,0 +1,18 @@
+package com.pc.apigateway.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/check")
+    public ResponseEntity<Boolean> healthCheck() {
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+}
